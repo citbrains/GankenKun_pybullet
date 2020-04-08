e7d9fab5242c005d5961650e1609efe219f74897
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2020-04-08 19:10:06 +0900

    add stretch.py

--- display.py
+++ display.py
@@ -6,17 +6,13 @@ from time import sleep
 TIME_STEP = 0.001
 
 physicsClient = p.connect(p.GUI)
-p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
 p.setGravity(0, 0, -9.8)
 p.setTimeStep(TIME_STEP)
 
-planeOrn = [0,0,0,1]
-planeId = p.loadURDF("URDF/plane.urdf", [0,0,0], planeOrn)
+planeId = p.loadURDF("URDF/plane.urdf", [0, 0, 0])
 RobotId = p.loadURDF("URDF/gankenkun.urdf", [0, 0, 0])
 
-numJoints = p.getNumJoints(RobotId)
-
-for joint in range(numJoints):
+for joint in range(p.getNumJoints(RobotId)):
   p.setJointMotorControl(RobotId, joint, p.POSITION_CONTROL, 0)
 
 while p.isConnected():
