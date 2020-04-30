4cd764f1480e4edb9c84254168ed4c164f66eaae
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2020-04-30 13:48:45 +0900

    foot_step_planner.py

--- GankenKun/foot_step_planner.py
+++ GankenKun/foot_step_planner.py
@@ -1,89 +1,80 @@
 #!/usr/bin/env python3
 # 
-# foot step planner for the GankenKun
+# foot step planner
 
 import math
 
 class foot_step_planner():
-  def __init__(self, dt, max_stride_x, max_stride_y, max_stride_th, period, width):
-    self.dt = dt
+  def __init__(self, max_stride_x, max_stride_y, max_stride_th, period, width):
     self.max_stride_x = max_stride_x
     self.max_stride_y = max_stride_y
     self.max_stride_th = max_stride_th
     self.period = period
     self.width = width
 
-  def calculate(self, goal_x,goal_y, goal_th, next_support_leg, status):
-    self.goal_x = goal_x
-    self.goal_y = goal_y
-    self.goal_th = goal_th
-    self.next_support_leg = next_support_leg
-    self.status = status
-    self.time = 0.0
-
+  def calculate(self, goal_x, goal_y, goal_th, next_support_leg, status):
     # calculate the number of foot step
-    goal_distance = math.sqrt(self.goal_x**2 + self.goal_y**2)
-    step_x  = abs(self.goal_x )/self.max_stride_x
-    step_y  = abs(self.goal_y )/self.max_stride_y
-    step_th = abs(self.goal_th)/self.max_stride_th
-    self.max_step = max(step_x, step_y, step_th)
-
-    self.stride_x  = self.goal_x /self.max_step
-    self.stride_y  = self.goal_y /self.max_step
-    self.stride_th = self.goal_th/self.max_step
-
-    foot_step = []
+    time = 0.0
+    goal_distance = math.sqrt(goal_x**2 + goal_y**2)
+    step_x  = abs(goal_x )/self.max_stride_x
+    step_y  = abs(goal_y )/self.max_stride_y
+    step_th = abs(goal_th)/self.max_stride_th
+    max_step = max(step_x, step_y, step_th)
+    stride_x  = goal_x /max_step
+    stride_y  = goal_y /max_step
+    stride_th = goal_th/max_step
 
     # first step
+    foot_step = []
     if status == 'start':
       foot_step += [[0.0, 0.0, 0.0, 0.0, 'both']]
-      self.time += self.period * 2.0
-    if self.next_support_leg == 'right':
-      foot_step += [[self.time, 0.0, -self.width, 0.0, self.next_support_leg]]
-      self.next_support_leg = 'left'
-    elif self.next_support_leg == 'left':
-      foot_step += [[self.time, 0.0,  self.width, 0.0, self.next_support_leg]]
-      self.next_support_leg = 'right'
+      time += self.period * 2.0
+    if next_support_leg == 'right':
+      foot_step += [[time, 0.0, -self.width, 0.0, next_support_leg]]
+      next_support_leg = 'left'
+    elif next_support_leg == 'left':
+      foot_step += [[time, 0.0,  self.width, 0.0, next_support_leg]]
+      next_support_leg = 'right'
 
     # walking
-    current_x = current_y = current_th = 0.0
     counter = 0
+    current_x = current_y = current_th = 0.0
     while True:
       counter += 1
-      diff_x  = abs(self.goal_x  - current_x )
-      diff_y  = abs(self.goal_y  - current_y )
-      diff_th = abs(self.goal_th - current_th)
+      diff_x  = abs(goal_x  - current_x )
+      diff_y  = abs(goal_y  - current_y )
+      diff_th = abs(goal_th - current_th)
       if diff_x <= self.max_stride_x and diff_y <= self.max_stride_y and diff_th <= self.max_stride_th:
         break
-      next_x  = current_x  + self.stride_x
-      next_y  = current_y  + self.stride_y
-      next_th = current_th + self.stride_th
-      self.time += self.period
-      if self.next_support_leg == 'right':
-        foot_step += [[self.time, next_x, next_y-self.width, next_th, self.next_support_leg]]
-        self.next_support_leg = 'left'
-      elif self.next_support_leg == 'left':
-        foot_step += [[self.time, next_x, next_y+self.width, next_th, self.next_support_leg]]
-        self.next_support_leg = 'right'
+      time += self.period
+      next_x  = current_x  + stride_x
+      next_y  = current_y  + stride_y
+      next_th = current_th + stride_th
+      if next_support_leg == 'right':
+        foot_step += [[time, next_x, next_y-self.width, next_th, next_support_leg]]
+        next_support_leg = 'left'
+      elif next_support_leg == 'left':
+        foot_step += [[time, next_x, next_y+self.width, next_th, next_support_leg]]
+        next_support_leg = 'right'
       current_x, current_y, current_the = next_x, next_y, next_th
 
     # last step
-    next_x, next_y, next_th = self.goal_x, self.goal_y, self.goal_th
+    next_x, next_y, next_th = goal_x, goal_y, goal_th
     if not status == 'stop':
-      self.time += self.period
-      if self.next_support_leg == 'right':
-        foot_step += [[self.time, next_x, next_y-self.width, next_th, self.next_support_leg]]
-      elif self.next_support_leg == 'left':
-        foot_step += [[self.time, next_x, next_y+self.width, next_th, self.next_support_leg]]
-      self.time += self.period
-      self.next_support_leg = 'both'
-      foot_step += [[self.time, next_x, next_y, next_th, self.next_support_leg]]
-      self.time += 2.0
-      foot_step += [[self.time, next_x, next_y, next_th, self.next_support_leg]]
+      time += self.period
+      if next_support_leg == 'right':
+        foot_step += [[time, next_x, next_y-self.width, next_th, next_support_leg]]
+      elif next_support_leg == 'left':
+        foot_step += [[time, next_x, next_y+self.width, next_th, next_support_leg]]
+      time += self.period
+      next_support_leg = 'both'
+      foot_step += [[time, next_x, next_y, next_th, next_support_leg]]
+      time += 2.0
+      foot_step += [[time, next_x, next_y, next_th, next_support_leg]]
 
     return foot_step
 
 if __name__ == '__main__':
-  planner = foot_step_planner(0.01, 0.06, 0.04, 0.1, 0.34, 0.044)
+  planner = foot_step_planner(0.06, 0.04, 0.1, 0.34, 0.044)
   foot_step = planner.calculate(0.3, 0.0, 0.0, 'right', 'start')
   print(foot_step)
